d301410731adc1e408ff4f407eedf88efc1455e5
Kien Mac Trung <kien.mac@niteco.se>
2024-02-26 17:13:27 +0700

    feat (setup): Setup tailwind, styled-components, eslint, prettier

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+import tw, { GlobalStyles as BaseStyles, theme } from 'twin.macro';
+
+const CustomStyles = createGlobalStyle({
+  body: {
+    WebkitTapHighlightColor: theme`colors.purple.500`,
+    ...tw`antialiased`,
+    fontFamily: "'Roboto', sans-serif",
+  },
+});
+
+const GlobalStyles = () => (
+  <>
+    <BaseStyles />
+    <CustomStyles />
+  </>
+);
+
+export default GlobalStyles;
